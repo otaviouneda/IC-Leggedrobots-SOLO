0a6b7843b60a9036c65a8d0d88296b394a05bf33
otaviouneda <126722121+otaviouneda@users.noreply.github.com>
2023-03-14 22:36:21 -0300

    Create ArduinoMPUAcelerômetro.cpp

---
+++ ArduinoMPUAcelerômetro.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <MPU9250.h>
+#include <Wire.h>
+
+MPU9250 IMU(Wire,0x68);
+
+
+void setup() {
+  Serial.begin(9600);
+  IMU.begin();
+}
+
+void loop() {
+  IMU.readSensor();
+  Serial.print(IMU.getAccelX_mss(), 6);
+  Serial.print('\n');
+  delay(1000);
+}
+
